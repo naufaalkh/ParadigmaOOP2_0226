c5ea4b6eefa974e770af16032880e34620b4ea4e
naufaalkh <naufaalkh@gmail.com>
2024-05-28 15:58:38 +0700

    membuat input dan output nilai

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -5,5 +5,14 @@ class AbstraksiKlas {
 private: string x, y;
 public:
     // method untuk mengisi nilai
-    // private member
+    // private 
+    void setXY(string a, string b){
+        x = a;
+        y = b;
+    }
+    // menampilkan nilai
+    void display(){
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
 };
