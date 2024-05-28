9487e25dba9eed8039eb0c779f2bce3bfe08f6cc
naufaalkh <naufaalkh@gmail.com>
2024-05-28 14:58:49 +0700

    membuat budi

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -31,3 +31,14 @@ class pelajar : public orang {
         cout << "pelajar dibuat\n" << endl;
     }
 };
+
+class budi : public pekerja, public pelajar {
+    public:
+
+    budi(int pUmur):
+    pekerja(pUmur),
+    pelajar(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
